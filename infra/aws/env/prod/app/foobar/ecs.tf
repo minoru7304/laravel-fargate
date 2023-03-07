b86604aaab6ea03fae219a9332927c9c44564a80
minoru7304 <minoru7304@gmail.com>
2023-03-07 11:10:23 +0900

    ECSオートスケーリング設定を追加

--- infra/aws/env/prod/app/foobar/ecs.tf
+++ infra/aws/env/prod/app/foobar/ecs.tf
@@ -10,3 +10,26 @@ resource "aws_ecs_cluster" "this" {
     Name = "${local.name_prefix}-${local.service_name}"
   }
 }
+
+resource "aws_appautoscaling_target" "ecsfargate" {
+  service_namespace  = "ecs"
+  resource_id        = "service/${aws_ecs_cluster.this.name}/example-prod-foobar"
+  scalable_dimension = "ecs:service:DesiredCount"
+  min_capacity       = 2
+  max_capacity       = 4
+}
+
+resource "aws_appautoscaling_policy" "ecsfargate_scale_out" {
+  name               = "scale_out"
+  policy_type        = "TargetTrackingScaling"
+  service_namespace  = aws_appautoscaling_target.ecsfargate.service_namespace
+  resource_id        = aws_appautoscaling_target.ecsfargate.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecsfargate.scalable_dimension
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value = 40.0
+  }
+}
